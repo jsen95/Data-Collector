405b9011b8f24077d27658ff7f6056d2af7af839
Justin Sen <jsen0795@gmail.com>
2019-09-25 00:07:03 -0500

    implemented success page

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/success")
+def success():
+	return render_template("success.html")
+
+
 if __name__ == '__main__':
 	app.debug=True
 	app.run()
