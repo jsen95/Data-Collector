d057bc1a79edfbcf9be044e1c9287f3a7499a665
Justin Sen <jsen0795@gmail.com>
2019-09-25 13:50:40 -0500

    created db model

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:justin0713@localhost/heightdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+	__tablename__="data"
+	id=db.Column(db.Integer, primary_key=True)
+	email_=db.Column(db.String(120), unique=True)
+	height_=db.Column(db.Integer)
+
+	def __init__(self, email_, height_):
+		self.email_= email_
+		self.height_= height_
 
 @app.route("/")
 def index():
